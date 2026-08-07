20e9a4d881a0f429f78ee1bd2e83150954e11010
igornik2007-byte <igornik2007@gmail.com>
2026-08-07 19:03:57 +0300

    Update checker.py

--- checker.py
+++ checker.py
@@ -47,6 +47,11 @@ def main():
     for author_url in authors:
 
         books = get_author_books(author_url)
+        print("Проверяем:", author_url)
+
+print("Найдено книг:", len(books))
+
+print(books[:5])
 
         old_books = seen.get(author_url, [])
 
